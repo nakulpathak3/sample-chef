a1a9524612cbe55c368fbbc785104772ce3b7371
Nakul Pathak <nakulpathak3@hotmail.com>
2016-01-16 17:14:34 -0500

    Update rails-demo

--- cookbooks/rails-demo/recipes/default.rb
+++ cookbooks/rails-demo/recipes/default.rb
@@ -6,3 +6,11 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+application "rails-demo" do
+  path '/var/www/rails-apps/rails-demo'
+  owner 'vagrant'
+  group 'vagrant'
+  repository "https://github.com/nakulpathak3/books-api-rails"
+  passenger_apache2  
+end
